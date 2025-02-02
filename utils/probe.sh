e0dab4278e986dda293794afde9b50a91923a094
Christian Lindig <lindig@gmail.com>
2025-02-02 18:16:43 +0000

    probe.sh: emit location, too
    Signed-off-by: Christian Lindig <lindig@gmail.com>

--- utils/probe.sh
+++ utils/probe.sh
@@ -7,5 +7,7 @@ for f in $@; do
   ffprobe -v quiet -print_format json -show_format "$f" |\
     jq -r '[.format.filename,
          .format.tags."com.apple.quicktime.creationdate",
-         .format.duration] | @csv'
+         .format.duration,
+         .format.tags."com.apple.quicktime.location.ISO6709"
+         ] | @csv'
 done
