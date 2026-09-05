e020587024eec70a89688055812058fa31be3c47
Christian Lindig <lindig@gmail.com>
2026-09-05 20:23:18 +0100

    Set default to join records

--- lib/fit.ml
+++ lib/fit.ml
@@ -687,7 +687,7 @@ end
 
 let to_json fit = `List (List.map JSON.record fit.records)
 
-let records ?(join = false) fit =
+let records ?(join = true) fit =
   fit.records
   |> List.filter_map Record.record
   |> if join then Record.join else Fun.id
